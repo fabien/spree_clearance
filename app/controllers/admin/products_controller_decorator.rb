9ab7cc8f0f3ecc46de4c30aea53c22a680731919
Fabien Franzen <info@atelierfabien.be>
2011-11-28 19:54:55 +0100

    Hacked around issue where callback handler was called twice

--- app/controllers/admin/products_controller_decorator.rb
+++ app/controllers/admin/products_controller_decorator.rb
@@ -1,15 +1,21 @@
 Admin::ProductsController.class_eval do
   
-  update.before :handle_marked_for_clearance
+  update.before :handle_marked_for_clearance 
+  
+  attr_accessor :handled_marked_for_clearance
   
   protected
   
-  def handle_marked_for_clearance
+  def handle_marked_for_clearance    
+    return if self.handled_marked_for_clearance # prevent double callback!
+    
     if (params[:product] || {}).delete(:marked_for_clearance).blank?
       @product.unmark_for_clearance
     else
       @product.mark_for_clearance
     end
+    
+    self.handled_marked_for_clearance = true
   end
   
 end
--- app/models/product_decorator.rb
+++ app/models/product_decorator.rb
@@ -8,12 +8,20 @@ Product.class_eval do
   scope :ascend_by_clearance, order("products.marked_for_clearance_at ASC, products.updated_at ASC")
   scope :descend_by_clearance, order("products.marked_for_clearance_at DESC, products.updated_at DESC")
   
-  def mark_for_clearance
+  def mark_for_clearance!
     self.update_attribute(:marked_for_clearance_at, Time.current)
+  end
+  
+  def mark_for_clearance
+    self.marked_for_clearance_at = Time.current
+  end
+
+  def unmark_for_clearance!
+    self.update_attribute(:marked_for_clearance_at, nil)
   end
 
   def unmark_for_clearance
-    self.update_attribute(:marked_for_clearance_at, nil)
+    self.marked_for_clearance_at = nil
   end
     
   def marked_for_clearance?
